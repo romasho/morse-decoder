de2a9d291ae30b05ed0ee638668e2d3f20380511
romasho <80854142+romasho@users.noreply.github.com>
2021-08-17 17:38:44 +0300

    feat: morse-decoder

--- src/index.js
+++ src/index.js
@@ -35,11 +35,32 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' '
 };
+function numbersInSymbols(nums) {
+    nums = nums.replace(/00/g , '')
+    nums = nums.replace(/10/g , '.')
+    nums = nums.replace(/11/g , '-')
+    return nums
+}
 
 function decode(expr) {
-    // write your solution here
-}
+    let chars =[]
+    let symbols = []
+    for (let i = 0; i < expr.length; i += 10) {
+       chars.push(expr.slice(i, i+10))
+    }
+    for (let char = 0; char < chars.length; char++) {
+       symbols.push(numbersInSymbols(chars[char]))
+    }
+    
+    let result = ""
+    for (let r = 0; r < symbols.length; r++) {
+        result = result + MORSE_TABLE[symbols[r]]
+    }
+    return result
+    }   
+   
 
 module.exports = {
     decode
